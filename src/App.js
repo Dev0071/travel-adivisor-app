4eee6b50fde4df0b146c42261fc3c474b6b09705
Dev0071 <lecturergacheru71@gmail.com>
2023-03-21 10:06:27 +0300

    layed out the folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Haeder';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
   return (
-    <div>
-      <h2>App</h2>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid
+        container
+        spacing={3}
+        style={{ width: '100%' }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={4}
+        >
+          <List />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+        >
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
---
+++ src/components/Header/Haeder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Haeder = () => {
+  return (
+    <div>
+      <h2>Header component</h2>
+    </div>
+  );
+};
+
+export default Haeder;
