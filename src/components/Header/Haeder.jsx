a15bc60280cbc78f48af625866f76e485b196c20
Dev0071 <lecturergacheru71@gmail.com>
2023-03-21 11:05:04 +0300

    finished laying out header.jsx

--- src/components/Header/Haeder.jsx
+++ src/components/Header/Haeder.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 const Haeder = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h2>Header component</h2>
-    </div>
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant='h5'
+          className={classes.title}
+        >
+          Travel advisor
+        </Typography>
+
+        <Box display='flex'>
+          <Typography
+            variant='h6'
+            className={classes.title}
+          >
+            Explore new Places
+          </Typography>
+
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='serach...'
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
